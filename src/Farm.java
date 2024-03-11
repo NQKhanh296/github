e5ec7f7e96b8acc71a693f2dc9dee36e5053e1ab
NQKhanh296 <khanhvn296@gmail.com>
2024-03-11 09:59:30 +0100

    added new setters and getters

--- src/Farm.java
+++ src/Farm.java
@@ -11,4 +11,28 @@ public class Farm {
         area = 100;
     }
 
+    public ArrayList<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(ArrayList<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public ArrayList<Flower> getFlowers() {
+        return flowers;
+    }
+
+    public void setFlowers(ArrayList<Flower> flowers) {
+        this.flowers = flowers;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+
 }
