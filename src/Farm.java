ecbbc3c4de18c8434105d21c5e2f265cfbefabf3
NQKhanh296 <khanhvn296@gmail.com>
2024-03-11 09:49:01 +0100

    added new constructor Farm

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+
 public class Farm {
+    private ArrayList<Animal> animals;
+    private ArrayList<Flower> flowers;
+    private int area;
 
-
+    public Farm() {
+        animals= new ArrayList<>();
+        flowers = new ArrayList<>();
+        area = 100;
+    }
 
 }
